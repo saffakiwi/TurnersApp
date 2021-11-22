84551850a2fe1c23717ecddf7cc5511a4e89bdd6
Ruan de Klerk <ruandk@missionreadyhq.com>
2021-11-22 16:55:05 +1300

    removed testing for now

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,4 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('insurance', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/insurance/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
